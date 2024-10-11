58ca590786171e236324d4bb851a8ade22260f0b
mach90 <rilem.benane@gmail.com>
2024-10-11 13:34:49 +0200

    parallax and contact section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,9 @@ export default function Footer() {
     const body = encodeURIComponent("Hello, are you availaible ?");
 
     const footerStyle = "px-6 py-6 lg:px-[10%] xl:px-[20%] flex flex-col justify-between items-end bg-lightPrimary dark:bg-darkPrimary bg-opacity-80 dark:bg-opacity-90 text-lightSubheading dark:text-darkSubheading font-heading text-center h-screen";
-    const footerContainerStyle = "flex flex-col justify-between items-center h-full";
+    const footerContainerStyle = "flex flex-col justify-between items-center h-full w-full";
     
-    const footerContainerUpperStyle = "flex flex-col gap-2 justify-center items-center h-full font-black";
+    const footerContainerUpperStyle = "flex flex-col gap-2 justify-center items-center h-full w-full font-black";
     const footerEnveloppe = "w-full h-full hover:scale-105 duration-200";
 
     const footerContainerLowerStyle = "flex flex-row gap-4 w-full justify-between border-t border-lightPrimaryBorder dark:border-darkPrimaryBorder pt-4";
@@ -25,9 +25,9 @@ export default function Footer() {
             <footer id="contact" className={footerStyle}>
                 <div className={footerContainerStyle}>
                     <div className={footerContainerUpperStyle}>
-                        <p className="text-lg">Seeking a dedicated developer eager to grow?</p>
-                        <h1 className="text-9xl z-10">Let's work together</h1>
-                        <a href={`mailto:${bill}.${gates}@${gmail}.com?subject=${subject}&body=${body}`} className="dark:border-darkPrimaryBorder w-80 rounded-lg">
+                        <p className="text-sm sm:text-md md:text-lg">Seeking a dedicated developer eager to grow?</p>
+                        <h1 className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl z-10">Let's work together</h1>
+                        <a href={`mailto:${bill}.${gates}@${gmail}.com?subject=${subject}&body=${body}`} className="w-40 lg:w-80 rounded-lg">
                             <img id="enveloppeSVG" src={EnveloppeSVG} alt="Enveloppe SVG" width="100%" className={footerEnveloppe}/>
                         </a>
                     </div>
@@ -39,13 +39,13 @@ export default function Footer() {
                             <a href="#" target="_blank" className={socialsItemStyle}><FaTwitter /></a>
                             <a href="#" target="_blank" className={socialsItemStyle}><FaStackOverflow /></a>
                             <a href="#" target="_blank" className={socialsItemStyle}><FaMedium /></a>
-                            <a href="#" target="_blank" className={socialsItemStyle}><FaYoutube /></a>
+                            {/* <a href="#" target="_blank" className={socialsItemStyle}><FaYoutube /></a>
                             <a href="#" target="_blank" className={socialsItemStyle}><FaInstagram /></a>
                             <a href="#" target="_blank" className={socialsItemStyle}><FaDribbble /></a>
                             <a href="#" target="_blank" className={socialsItemStyle}><FaCodepen /></a>
                             <a href="#" target="_blank" className={socialsItemStyle}><Fa500Px /></a>
                             <a href="#" target="_blank" className={socialsItemStyle}><FaUnsplash /></a>
-                            <a href="#" target="_blank" className={socialsItemStyle}><FaArtstation /></a>
+                            <a href="#" target="_blank" className={socialsItemStyle}><FaArtstation /></a> */}
                         </div>
                     </div>
                 </div>
